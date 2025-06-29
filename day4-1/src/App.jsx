2bc652ed76b56bd241c537ddb21204c25fc7a509
Quadeer-Q <khajaquadeerahmed@gmail.com>
2025-06-29 20:49:45 +0000

    not completed yet

---
+++ day4-1/src/App.jsx
@@ -0,0 +1,66 @@
+import { useState, useCallback } from 'react'
+
+function App() {
+
+  const [length, setlength]= useState(8)
+  const [numberallowed, setNumberAllowed] =useState(false)
+  const [charallowed, setCharAllowed]= useState(false)
+  const [password, setPassword ]= useState("")
+
+  const passwordGenerator= useCallback( () => {
+    let pass=""
+    let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if(numberallowed) str+= "0123456789"
+    if(charallowed) str+= "!@#$%^&*()_+[]{}|;:,.<>?/~`"
+
+    for(let i=0; i<length; i++){
+      let char=Math.floor(Math.random()*str.length)
+      pass+=str.charAt(char)
+    }
+    setPassword(pass)
+
+  }, [length, numberallowed, charallowed, setPassword])
+
+
+  return (
+    <>
+      <div className= 'w-full max-w-md mx-auto shadow-md rounded-lg  px-4 my-8 text-orange-500 bg-gray-700 py-4'> <h1 className='text-white text-center'>Password Generator </h1> 
+      <div className= 'flex shadow rounded-lg overflow-hidden bg-white mb-4 my-4  '>
+        <input type="text"
+        value={password}
+        className= 'outline-none w-full px-4 py-2 text-white-900 '
+        placeholder='Generated Password'
+        />
+
+        </div>
+        <div className='flex text-sm gap-x-2'>
+          <div className=' flex items-center gap-x-2'>
+            <input type='range'
+            min={6}
+            max={100}
+            value={length}
+            className='cursor-pointer'
+            onChange= {(e)=>{setlength(e.target.value)}}
+            />
+            <label>length: {length}</label>
+         </div>
+        </div>
+        <div className='flex items-center gap-x-2'>
+          <input type='checkbox'
+          checked={numberallowed}
+           onChange= {()=> {setNumberAllowed((prev)=>!prev)}}
+          />
+          <label>Include Numbers</label>
+        </div>
+        <div className='flex items-center gap-x-2'>
+          <input type='checkbox'
+          checked={charallowed}
+          onChange= {()=> {setCharAllowed((prev)=>!prev)}}
+          />
+          <label>Include Special Characters</label>
+          </div>  
+      </div>
+    </>
+  )
+}
+export default App
